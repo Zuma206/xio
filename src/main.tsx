e325063c0247bde5dd9800f15fda90c1316d5b83
Zuma206 <Zuma#2517>
2022-11-09 21:17:16 +0000

    Added basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./pages/App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
