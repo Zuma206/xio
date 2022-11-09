2cc8a57efabd4965260416a24adc9cd3989cd898
Zuma206 <Zuma#2517>
2022-11-09 21:21:26 +0000

    added global css and 404 route

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
+import PageNotFound from "./pages/404";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
