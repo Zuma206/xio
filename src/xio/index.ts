8cbae381a9c60af7e452e1b44fbdabfc02296c73
Zuma206 <Zuma#2517>
2022-11-10 19:45:07 +0000

    Added getGravatar function to api

--- src/xio/index.ts
+++ src/xio/index.ts
@@ -1,1 +1,8 @@
+import md5 from "md5";
+
 export * from "./authContext";
+
+export const getGravatar = (email: string) => {
+    const hash = md5(email);
+    return `https://www.gravatar.com/avatar/${hash}`;
+};
