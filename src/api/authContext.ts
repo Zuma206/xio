04d03c080f5a1301c000e0561a71176441d8861c
Zuma <james.peedle@yahoo.com>
2022-11-10 01:16:43 +0000

    Refined auth context logic

--- src/api/authContext.ts
+++ src/api/authContext.ts
@@ -1,6 +1,14 @@
 import { User } from "firebase/auth";
-import { createContext, useContext, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useContext } from "react";
 
-export const AuthContext = createContext(useState<null | User>(null));
+// Type that represents a state array for User|null
+export type AuthState = [User | null, Dispatch<SetStateAction<User | null>>];
+
+// Create an AuthContext for storing user data
+export const AuthContext = createContext<AuthState>([null, () => {}]);
+
+// Create a hook-like function to returns the user data from the context
 export const useAuth = () => useContext(AuthContext)[0];
-export const setAuth = (auth: null | User) => useContext(AuthContext)[1](auth);
+
+// Returns the setter function for the auth state
+export const getAuthSetter = () => useContext(AuthContext)[1];
