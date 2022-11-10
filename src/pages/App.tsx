dc479f5a2cbb7ad00071b08535b5989897847092
Zuma <james.peedle@yahoo.com>
2022-11-10 00:48:54 +0000

    Provided auth context

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import HeaderBar from "../components/HeaderBar";
 import Columns from "../components/Columns";
+import { AuthContext } from "../api";
+import { User } from "firebase/auth";
 
 export default () => {
+    const authState = useState<null | User>(null);
+
     return (
-        <HeaderBar>
-            <Columns>
-                <div>Servers</div>
-                <div>Messages</div>
-            </Columns>
-        </HeaderBar>
+        <AuthContext.Provider value={authState}>
+            <HeaderBar>
+                <Columns>
+                    <div>Servers</div>
+                    <div>Messages</div>
+                </Columns>
+            </HeaderBar>
+        </AuthContext.Provider>
     );
 };
