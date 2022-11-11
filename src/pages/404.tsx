2bf82f3e3fd9c0fbf278266c93c4986599c95fd6
Zuma206 <Zuma#2517>
2022-11-11 18:33:11 +0000

    Added 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,18 @@
+import HeaderBar from "../components/HeaderBar";
+import styles from "../styles/404.module.scss";
+import { Link } from "react-router-dom";
+
 export default () => {
-    return <h1>404</h1>;
+    return (
+        <HeaderBar>
+            <div className={styles.notFound}>
+                <h1>Route not found :/</h1>
+                <Link to="/">
+                    <button className={styles.button}>
+                        Take me somewhere real
+                    </button>
+                </Link>
+            </div>
+        </HeaderBar>
+    );
 };
