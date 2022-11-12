22f4aff79cb005d6f5cce5519ebbe2bdc23eda3a
Zuma206 <Zuma#2517>
2022-11-12 14:00:18 +0000

    Re-formatted content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,19 +1,28 @@
 import { useXIOUser } from "../xio";
 import AccountSetup from "./AccountSetup";
+import styles from "../styles/Content.module.scss";
 
 export default () => {
     const [user] = useXIOUser();
+    const loading = <div className={styles.padded}>Loading...</div>;
+
     return user == "unknown" ? (
-        <div>Loading...</div>
+        loading
     ) : user == "known" ? (
-        <>
+        <div className={styles.padded}>
             <h1>Welcome to XIO</h1>
             <p>To continue, please sign in</p>
-        </>
+        </div>
     ) : user.activated == "activated" ? (
-        <h1>Welcome, {user.username}</h1>
+        <div className={styles.padded}>
+            <h1>Welcome, {user.username}</h1>
+            <p>
+                You've been signed in successfully, and your account is fully
+                setup
+            </p>
+        </div>
     ) : user.activated == "unknown" ? (
-        <div>Loading...</div>
+        loading
     ) : (
         <AccountSetup />
     );
