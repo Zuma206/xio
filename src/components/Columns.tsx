8aeb06a314f75deeae6fdfc2c1de6e066d786e8a
Zuma <james.peedle@yahoo.com>
2022-12-11 03:40:26 +0000

    made starting column correct on mobile

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Columns.module.scss";
 
 export default ({ children }: React.PropsWithChildren) => {
     // Take first 2 children to place in left and right columns
     const [Left, Right] = React.Children.toArray(children);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-    return (
+    useEffect(() => {
+        window.addEventListener("resize", (e) => {
+            setScreenWidth(window.innerWidth);
+        });
+    }, []);
+
+    return screenWidth > 900 ? (
         <div className={styles.container}>
             <div className={styles.left}>{Left}</div>
             <div className={styles.right}>{Right}</div>
+        </div>
+    ) : (
+        <div className={styles.container}>
+            <div className={styles.right}>{Right}</div>
+            <div className={styles.left}>{Left}</div>
         </div>
     );
 };
