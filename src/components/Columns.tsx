a2cc47dc7c7bac5bf0a07b80ee6b7717a050413f
Zuma206 <Zuma#2517>
2023-01-15 18:07:16 +0000

    conditionally rendered sidebar

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Columns.module.scss";
+import { useXIOUser } from "../xio";
 
 export default ({ children }: React.PropsWithChildren) => {
     // Take first 2 children to place in left and right columns
     const [Left, Right] = React.Children.toArray(children);
     const mainRef = useRef<HTMLDivElement | null>(null);
+    const [user] = useXIOUser();
 
     useEffect(() => {
         if (!mainRef.current) return;
@@ -13,7 +15,9 @@ export default ({ children }: React.PropsWithChildren) => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.left}>{Left}</div>
+            {user == "unknown" || user == "known" ? null : (
+                <div className={styles.left}>{Left}</div>
+            )}
             <div className={styles.right} ref={mainRef}>
                 {Right}
             </div>
