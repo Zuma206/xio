5bb95e9b080bb9dd88a7e88245afb90c14ee885b
Zuma206 <Zuma#2517>
2023-01-15 19:00:32 +0000

    fixed twemoji

--- src/components/MessageContent.tsx
+++ src/components/MessageContent.tsx
@@ -5,51 +5,47 @@ import { RefObject } from "react";
 import Twemoji from "react-twemoji";
 
 interface Props {
-    content: string;
-    scroll: boolean;
-    scrollDirection: "up" | "down";
-    end: RefObject<HTMLDivElement>;
+  content: string;
+  scroll: boolean;
+  scrollDirection: "up" | "down";
+  end: RefObject<HTMLDivElement>;
 }
 
-export default ({ content, scroll, scrollDirection, end }: Props) => {
-    const parsedMessage = parseMessage(content);
-    return (
-        <div className={styles.container}>
-            <Twemoji
-                options={{
-                    className:
-                        parsedMessage.length > 1
-                            ? styles.twemoji
-                            : styles.bigTwemoji,
-                }}
-            >
-                {parsedMessage.map(({ type, value: token }, index) => {
-                    switch (type) {
-                        case "text":
-                            return token;
-                        case "space":
-                            return <span key={index}>&nbsp;</span>;
-                        case "link":
-                            return (
-                                <a
-                                    key={index}
-                                    target="_blank"
-                                    className={styles.link}
-                                    href={token}
-                                >
-                                    {token}
-                                </a>
-                            );
-                        case "emoji":
-                            const emojiToken = token.substring(
-                                1,
-                                token.length - 1
-                            );
-                            if (!emoji.hasEmoji(emojiToken)) return token;
-                            return emoji.get(emojiToken);
-                    }
-                })}
-            </Twemoji>
-        </div>
-    );
+export default ({ content }: Props) => {
+  const parsedMessage = parseMessage(content);
+  return (
+    <div className={styles.container}>
+      <Twemoji
+        options={{
+          className:
+            parsedMessage.length > 1 ? styles.twemoji : styles.bigTwemoji,
+          base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
+        }}
+      >
+        {parsedMessage.map(({ type, value: token }, index) => {
+          switch (type) {
+            case "text":
+              return token;
+            case "space":
+              return <span key={index}>&nbsp;</span>;
+            case "link":
+              return (
+                <a
+                  key={index}
+                  target="_blank"
+                  className={styles.link}
+                  href={token}
+                >
+                  {token}
+                </a>
+              );
+            case "emoji":
+              const emojiToken = token.substring(1, token.length - 1);
+              if (!emoji.hasEmoji(emojiToken)) return token;
+              return emoji.get(emojiToken);
+          }
+        })}
+      </Twemoji>
+    </div>
+  );
 };
