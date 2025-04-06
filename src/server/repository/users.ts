5f0fda09a4d4e52fda4f5e004d58e6877f245fe2
James <zuma@zuma.eu.org>
2025-04-07 00:41:29 +0100

    Updated account creation

--- src/server/repository/users.ts
+++ src/server/repository/users.ts
@@ -1,8 +1,12 @@
-import { eq } from "drizzle-orm";
+import { eq, InferInsertModel } from "drizzle-orm";
 import { db } from "../database/connection";
 import { users } from "../database/schema";
 import { first } from "./utils";
 
 export function getUserById(id: string) {
   return first(db.select().from(users).where(eq(users.id, id)));
+}
+
+export function insertUser(user: InferInsertModel<typeof users>) {
+  return db.insert(users).values(user);
 }
