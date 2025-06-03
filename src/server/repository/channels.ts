c134f5c4641cc48197961ff9f868deff7bf129f4
James <zuma@zuma.eu.org>
2025-06-03 19:58:33 +0100

    Updated isInChannel to include user_in_channel

--- src/server/repository/channels.ts
+++ src/server/repository/channels.ts
@@ -1,8 +1,8 @@
-import { eq, InferInsertModel } from "drizzle-orm";
+import { eq, exists, InferInsertModel, and, sql } from "drizzle-orm";
 import { db, Transaction } from "../database/connection";
-import { channels } from "../database/schema";
+import { channels, userInChannel, users } from "../database/schema";
 import { first, generateId } from "./utils";
-import { getUserById } from "./users";
+import { union } from "drizzle-orm/sqlite-core";
 
 export function getChannels(userId: string) {
   return db.select().from(channels).where(eq(channels.ownerId, userId));
@@ -27,7 +27,43 @@ function getChannelById(channelId: string, tx: Transaction = db) {
   return first(tx.select().from(channels).where(eq(channels.id, channelId)));
 }
 
-export async function isInChannel(userId: string, channelId: string) {
-  const channel = await getChannelById(channelId);
-  return channel?.ownerId == userId;
+export async function isInChannel(
+  userId: string,
+  channelId: string,
+  tx: Transaction = db
+) {
+  return Boolean(
+    await first(
+      tx
+        .select({ exists: sql`1` })
+        .from(users)
+        .where(
+          and(
+            eq(users.id, userId),
+            exists(
+              union(
+                tx
+                  .select({ exists: sql`1` })
+                  .from(channels)
+                  .where(
+                    and(
+                      eq(channels.ownerId, userId),
+                      eq(channels.id, channelId)
+                    )
+                  ),
+                tx
+                  .select({ exists: sql`1` })
+                  .from(userInChannel)
+                  .where(
+                    and(
+                      eq(userInChannel.userId, userId),
+                      eq(userInChannel.channelId, channelId)
+                    )
+                  )
+              )
+            )
+          )
+        )
+    )
+  );
 }
